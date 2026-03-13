937c3c51a2ff99a0be16439129c7b6031059e5ba
Tobiloba Oyeniyi <oyeniyitobiloba12@gmail.com>
2026-03-13 22:26:00 +0100

    added the break-all classname

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -5,13 +5,15 @@ import delete_icon from '../assets/delete.png'
 
 const TodoItems = ({text, id, isComplete, deleteTodo, toggle}) => {
   return (
-    <div onClick={() => {toggle(id)}} className='flex items-center my-3 gap-2'>
-        <div className='flex flex-1'>
+    <div onClick={() => {toggle(id)}} className='flex items-center justify-between my-3 gap-2'>
+        <div className='flex items-center gap-3 flex-1'>
             <img src={isComplete ? tick : not_tick} alt=""  className='w-7'/>
-            <p className={`text-slate-700 ml-4 text-[17px] decoration-slate-500 ${isComplete ?"line-through" : "" } `}>{text}</p>
+            <p className={`text-slate-700 ml-4 text-[17px] decoration-slate-500 flex-1 break-all ${isComplete ?"line-through" : "" } `}>{text}</p>
         </div>
 
-        <img onClick={() => {deleteTodo(id)}} src={delete_icon} alt="" className='w-3.5 cursor-pointer' />
+        <img onClick={(e) => {
+          e.stopPropagation();
+          deleteTodo(id)}} src={delete_icon} alt="" className='w-3.5 cursor-pointer' />
 
     </div>
   )
